71700b128b0ddfaf4993cda0b74c802fa24b3763
Mike Echevarria <michael.echevarria@sapns2.com>
2019-10-16 13:17:46 -0400

    add env variables

--- docker-run.sh
+++ docker-run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-
+# HXE_HOST defaults to hxehost and HXE_PORT defaults to 51006
 port=8080
 
 docker run \
     -p ${port}:${port} \
     -e PORT=${port} \
+    -e HXE_HOST=${HXE_HOST} \
+    -e HXE_PORT=${HXE_PORT}
     localhost/hxe_angular
