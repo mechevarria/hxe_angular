dcdd8a0cc615d3185aee25927c4dbcb4f2fa87a1
Mike Echevarria <michael.echevarria@sapns2.com>
2019-11-18 15:29:54 -0500

    show data in word cloud

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AnalysisService } from './analysis.service';
+import { CloudData, CloudOptions, TagCloudComponent } from 'angular-tag-cloud-module';
 
 @Component({
   selector: 'app-analysis',
@@ -7,17 +8,24 @@ import { AnalysisService } from './analysis.service';
   styleUrls: ['./analysis.component.css']
 })
 export class AnalysisComponent implements OnInit {
+  @ViewChild(TagCloudComponent, {static: false}) tagCloudComponent: TagCloudComponent;
+
   selected: string = '';
   types: string[] = [];
   limit: number = 30;
-  results: any = {};
+  options: CloudOptions = {
+    height: 600,
+    overflow: false
+  }
+  data: CloudData[] = [];
 
   constructor(private analysisSerivce: AnalysisService) { }
 
   update(): void {
     console.log(`limit is ${this.limit} and selected is ${this.selected}`);
     this.analysisSerivce.getAnalysis(this.selected, this.limit).subscribe(res => {
-      this.results = res;
+      this.data = res;
+      this.tagCloudComponent.reDraw();
     })
 
   }
