53084a4e277a8fc72c4d0366f967605ca3729149
Mike Echevarria <michael.echevarria@sapns2.com>
2019-11-26 13:19:43 -0500

    move docker push to build script

--- cf-push.sh
+++ cf-push.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-docker push quay.io/mechevarria/hxe_angular
-
 cf push hxe_angular --docker-image quay.io/mechevarria/hxe_angular
--- docker-build.sh
+++ docker-build.sh
@@ -4,3 +4,5 @@ docker build \
   --build-arg HXE_HOST=${HXE_HOST} \
   --build-arg HXE_PORT=${HXE_PORT} \
   -t quay.io/mechevarria/hxe_angular .
+
+docker push quay.io/mechevarria/hxe_angular
